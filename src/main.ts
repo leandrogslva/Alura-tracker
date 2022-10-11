58467b719ceb5faa37a5826c3a308b1179efd071
Leandro <leou.boy80@gmail.com>
2022-10-11 15:24:15 -0300

    feat: Importando e utilizando o store.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import roteador from './roteador'
+import { store, key } from './store'
 createApp(App)
     .use(roteador)
+    .use(store, key)
     .mount('#app')
