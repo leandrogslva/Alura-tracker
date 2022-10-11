2b75e963e5d655e51212cd7eb8323eca02baf940
Leandro <leou.boy80@gmail.com>
2022-10-11 15:26:35 -0300

    refactor: Isolando as responsabilidades em componentes.

---
+++ src/views/Projetos/Formulario.vue
@@ -0,0 +1,65 @@
+<template>
+  <section>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+import { useStore } from "@/store";
+
+export default defineComponent({
+  name: "FormularioProjetoView",
+  props: {
+    id: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      nomeDoProjeto: "",
+    };
+  },
+  methods: {
+    salvar() {
+      if (this.id) {
+        this.store.commit("ALTERA_PROJETO", {
+          id: this.id,
+          nome: this.nomeDoProjeto,
+        });
+      } else {
+        this.store.commit("ADICIONA_PROJETO", this.nomeDoProjeto);
+      }
+      this.nomeDoProjeto = "";
+      this.$router.push("/");
+    },
+  },
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+  mounted() {
+    if (this.id) {
+      const projeto = this.store.state.projetos.find(
+        (projeto) => projeto.id == this.id
+      );
+      this.nomeDoProjeto = projeto?.nome || "";
+    }
+  },
+});
+</script>
