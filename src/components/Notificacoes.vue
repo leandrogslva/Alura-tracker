05ba329b310de792e7c597a391baf9b941ec28c1
Leandro <leou.boy80@gmail.com>
2022-10-12 12:19:00 -0300

    feat: Componente de notificação.

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="notificacoes">
+    <article
+      class="message is-success"
+      :class="contexto[notificacao.tipo]"
+      v-for="notificacao in notificacoes"
+      :key="notificacao.id"
+    >
+      <div class="message-header">
+        <p>{{ notificacao.titulo }}</p>
+      </div>
+      <div class="message-body">
+        {{ notificacao.texto }}
+      </div>
+    </article>
+  </div>
+</template>
+
+<script lang="ts">
+import { TipoNotificacao } from "@/interfaces/INotificacao";
+import { useStore } from "@/store";
+import { computed, defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "NotificacoesApp",
+  data() {
+    return {
+      contexto: {
+        [TipoNotificacao.SUCESSO]: "is-sucess",
+        [TipoNotificacao.ATENCAO]: "is-warning",
+        [TipoNotificacao.FALHA]: "is-danger",
+      },
+    };
+  },
+  setup() {
+    const store = useStore();
+    return {
+      notificacoes: computed(() => store.state.notificacoes),
+    };
+  },
+});
+</script>
+
+<style scoped>
+.notificacoes {
+  position: absolute;
+  right: 0;
+  width: 300px;
+  padding: 8px;
+  z-index: 105f;
+}
+</style>
