b159b6702b648e1a112d9c57cc80cb86d8966067
Leandro <leou.boy80@gmail.com>
2022-10-16 13:25:47 -0300

    feat: Criando modal e métodos de controle do modal para edição de tarefas.

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,9 +1,48 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
-    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <Tarefa
+      v-for="(tarefa, index) in tarefas"
+      :key="index"
+      :tarefa="tarefa"
+      @aoTarefaClicada="selecionarTarefa"
+    />
+    <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje </Box>
+    <div
+      class="modal"
+      :class="{ 'is-active': tarefaSelecionada }"
+      v-if="tarefaSelecionada"
+    >
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head" style="height: 5rem">
+          <p class="modal-card-title">Modal title</p>
+          <button
+            class="delete"
+            aria-label="close"
+            @click="fecharModal"
+          ></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="field">
+            <label for="descricaoTarefa" class="label"> Descrição </label>
+            <input
+              type="text"
+              class="input"
+              v-model="tarefaSelecionada.descricao"
+              id="descricaoTarefa"
+            />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success" @click="alterarTarefa">
+            Salvar alterações
+          </button>
+          <button class="button" @click="fecharModal">Cancelar</button>
+        </footer>
+      </div>
+    </div>
   </div>
-  <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje </Box>
 </template>
 
 <script lang="ts">
@@ -12,11 +51,21 @@ import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
 import { useStore } from "@/store";
-import { CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from "@/store/tipo-acoes";
+import {
+  ALTERAR_TAREFA,
+  CADASTRAR_TAREFA,
+  OBTER_PROJETOS,
+  OBTER_TAREFAS,
+} from "@/store/tipo-acoes";
 import ITarefa from "@/interfaces/ITarefa";
 
 export default defineComponent({
   name: "TarefasViw",
+  data() {
+    return {
+      tarefaSelecionada: null as ITarefa | null,
+    };
+  },
   components: {
     Formulario,
     Tarefa,
@@ -25,6 +74,17 @@ export default defineComponent({
   methods: {
     salvarTarefa(tarefa: ITarefa): void {
       this.store.dispatch(CADASTRAR_TAREFA, tarefa);
+    },
+    selecionarTarefa(tarefa: ITarefa) {
+      this.tarefaSelecionada = Object.assign({}, tarefa);
+    },
+    fecharModal(): void {
+      this.tarefaSelecionada = null;
+    },
+    alterarTarefa() {
+      this.store
+        .dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal());
     },
   },
   computed: {
