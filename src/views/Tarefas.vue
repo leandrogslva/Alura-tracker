34bb85737c580533e63dc5573b88e9c15624c033
Leandro <leou.boy80@gmail.com>
2022-10-17 10:10:57 -0300

    feat: Filtro de tarefas

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,6 +1,19 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input
+          class="input"
+          type="text"
+          placeholder="Digite para filtrar"
+          v-model="filtro"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
     <Tarefa
       v-for="(tarefa, index) in tarefas"
       :key="index"
@@ -46,7 +59,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
@@ -66,16 +79,12 @@ export default defineComponent({
     Tarefa,
     Box,
   },
-  // computed: {
-  //   listaEstaVazia(): boolean {
-  //     return this.tarefas.length === 0;
-  //   },
-  // },
   setup() {
     const store = useStore();
     store.dispatch(OBTER_TAREFAS);
     store.dispatch(OBTER_PROJETOS);
     const tarefaSelecionada = ref(null as ITarefa | null);
+    const filtro = ref("");
 
     const salvarTarefa = (tarefa: ITarefa): void => {
       store.dispatch(CADASTRAR_TAREFA, tarefa);
@@ -91,7 +100,11 @@ export default defineComponent({
         .dispatch(ALTERAR_TAREFA, tarefaSelecionada.value)
         .then(() => fecharModal());
     };
-    
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value);
+    })
+
     return {
       store,
       fecharModal,
@@ -101,6 +114,7 @@ export default defineComponent({
       tarefaSelecionada,
       tarefas: computed(() => store.state.tarefa.tarefas),
       listaEstaVazia: computed(() => store.state.tarefa.tarefas.length === 0),
+      filtro,
     };
   },
 });
